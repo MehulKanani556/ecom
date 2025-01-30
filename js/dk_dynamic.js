ea9d0575bcc93678d3c4bfc39d9eda83bc239594
darshit2809 <darshitkalathiyainfotech@gmail.com>
2025-01-30 18:02:43 +0530

    darshit30-01

--- js/dk_dynamic.js
+++ js/dk_dynamic.js
@@ -117,31 +117,31 @@ const exploremore = [
         id: "2", 
         img: "women.png", 
         name: "Womens", 
-        link: "" 
+        link: "topwear" 
     },
     { 
         id: "3", 
         img: "kids.png", 
         name: "Kids", 
-        link: "" 
+        link: "topwear" 
     },
     { 
         id: "4", 
         img: "footwear.png", 
         name: "Footewear", 
-        link: "" 
+        link: "topwear" 
     },
     { 
         id: "5", 
         img: "fitness_equipment.png", 
         name: "Fitness Equipment", 
-        link: "" 
+        link: "topwear" 
     },
     { 
         id: "6", 
         img: "sports_accessories.png", 
         name: "Sports Accessories", 
-        link: "" 
+        link: "topwear" 
     },
 ];
 
@@ -217,19 +217,213 @@ const new_on_quickcart = [
 const newquickcart = new_on_quickcart.map((item, index) => {
   return `
   <div class="swiper-slide">
+                        <a href="/darshit/${item.link}.html">
                             <div class="card">
-                                <img src="../mv_image/new_under_armour.png" class="card-img-top" alt="Calvin Klein">
+                                <img src="../mv_image/${item.img1}" class="card-img-top" alt="Calvin Klein">
                                
                                 <div class="card-body">
                                     <div>
-                                        <img src="../mv_image/logo_under_armour.png" alt="" class="mx-auto" style="height: 25px;">
+                                        <img src="../mv_image/${item.img2}" alt="" class="mx-auto" style="height: 25px;">
                                     </div>
-                                    <p style="font-size: 12px; line-height: 1" class="card-text text-center mb-0">Up to 20% OFF</p>
-                                    <small class="text-center mv_trending_card_dis">T&C Apply</small>
+                                    <p style="font-size: 12px; line-height: 1" class="card-text text-center mb-0">Up to ${item.discount}% OFF</p>
+                                    <small class="text-center mv_trending_card_dis">${item.info}</small>
                                 </div>
                             </div>
-                        </div> 
+                            </a>
+                        </div>
                         `;
 }).join("");
 
 document.getElementById("dk_new_on_quick").innerHTML = newquickcart;
+
+// SportsWear page Trending Deals
+const trending_dealing = [
+    { 
+        id: "1", 
+        images: ["new_balance.png","new_balance.png","new_balance.png","new_balance.png","new_balance.png"],
+        name: "New Balance",
+        categorydesc: "Men's Fresh Foam X 1080v14",
+        thumbnail: ["new_balance1.png", "new_balance2.png", "new_balance3.png"],
+        discount: "40",
+        price: "700",
+        link:""
+    },
+    { 
+        id: "2", 
+        images: ["trending_reebok.png", "trending_reebok.png", "trending_reebok.png", "trending_reebok.png", "trending_reebok.png"],
+        name: "Reebok",
+        categorydesc: "Women's Running App",
+        thumbnail: ["trending_reebok1.png"],
+        discount: "50",
+        price: "399",
+        link:""
+    },
+    { 
+        id: "3", 
+        images: ["trending_skechers.png", "trending_skechers.png", "trending_skechers.png", "trending_skechers.png", "trending_skechers.png"],
+        name: "SKECHERS",
+        categorydesc: "Unisex Adults SK FIT YOGA MAT..",
+        thumbnail: ["trending_skechers1.png", "trending_skechers1.png"],
+        discount: "40",
+        price: "299",
+        link:""
+    }
+];
+
+const trending_deals = trending_dealing.map(item => {
+    return `
+        <div class="swiper-slide">
+            <div class="product-card w-100">
+                <div class="swiper innerSwiper">
+                    <div class="swiper-wrapper">
+                        ${item.images.map(img => 
+                            `<div class="swiper-slide">
+                                <img src="../mv_image/${img}" alt="${item.name} Image" class="r_v1img">
+                            </div>`
+                        ).join('')}
+                    </div>
+                    <div class="swiper-pagination"></div>
+                </div>
+                <h5 class="mv_tsmh">${item.name}</h5>
+                <p class="mv_tssh text-start">${item.categorydesc}</p>
+                <div class="r_thumbnailimg d-flex align-items-center">
+                    ${item.thumbnail.map(thumb => 
+                        `<img src="../mv_image/${thumb}" alt="${item.name} Thumbnail">`
+                    ).join('')}
+                </div>
+                <div class="mv_discount_text">${item.discount}% OFF</div>
+                <p class="text-start m-0">
+                    <strong class="mv_less_discount_price">$${item.price - (item.price * (item.discount / 100)).toFixed(0)}</strong>
+                    <del class="mv_without_discount_price">$${item.price}</del>
+                </p>
+            </div>
+        </div>
+    `;
+}).join(''); 
+
+document.getElementById("dk_trending_deals").innerHTML = trending_deals;
+
+// SportsWear page Trending in sports Accessories
+const sportswer_accessories = [
+    { 
+        id: "1", 
+        img: "trending_1.png", 
+        discount: "20", 
+        info:"*T&C Apply",
+        link: "topwear"
+    },
+    { 
+        id: "2", 
+        img: "trending_2.png", 
+        discount: "20", 
+        info:"*T&C Apply",
+        link: "topwear" 
+    },
+    { 
+        id: "3", 
+        img: "trending_3.png", 
+        discount: "20", 
+        info:"*T&C Apply",
+        link: "topwear"
+    },
+    { 
+        id: "4", 
+        img: "trending_4.png", 
+        discount: "20", 
+        info:"*T&C Apply",
+        link: "topwear" 
+    },
+    { 
+        id: "5", 
+        img: "trending_5.png", 
+        discount: "20", 
+        info:"*T&C Apply",
+        link: "topwear" 
+    },
+    { 
+        id: "6", 
+        img: "trending_1.png", 
+        discount: "20", 
+        info:"*T&C Apply",
+        link: "topwear"
+    },
+];
+
+const sportswer_acc = sportswer_accessories.map((item, index) => {
+                    return `
+                        <div class="swiper-slide">
+                        <a href="/darshit/${item.link}.html">
+                            <div class="card">
+                                <img src="../mv_image/${item.img}" class="card-img-top" alt="Calvin Klein">
+                               
+                                <div class="card-body">
+                                    <p style="font-size: 12px; line-height: 1" class="card-text text-center">Up to ${item.discount}% OFF</p>
+                                    <span class="text-center mv_trending_card_dis">T&C Apply</span>
+                                </div>
+                            </div>
+                            </a>
+                        </div>
+                        `;
+                    }).join("");
+
+document.getElementById("dk_trending_sports_acc").innerHTML = sportswer_acc;
+
+//SportsWear page Explore more box
+const sportswer_exporing_box = [
+    { 
+        id: "1", 
+        img: "sport_explore_1.png", 
+        name:"NEW STYLES",
+        btn:"Shop Now",
+        link: "topwear"
+    },
+    { 
+        id: "2", 
+        img: "sport_explore_2.png", 
+        name:"",
+        btn:"Shop Now",
+        showFinalTouch:"true",
+        link: "topwear" 
+    },
+    { 
+        id: "3", 
+        img: "sport_explore_3.png", 
+        name:"NO REST, JUST RUN",
+        description:"Run to live. Live to run",
+        btn:"Shop Now",
+        link: "topwear"
+    },
+    { 
+        id: "4", 
+        img: "sport_explore_4.png", 
+        name:"SPORTS ACCESSORIES",
+        description:"Your comfort, our Priority",
+        btn:"Shop Now",
+        link: "topwear" 
+    },
+];
+
+const sportswer_ex_box = sportswer_exporing_box.map((item, index) => {
+    return `
+       <div class="col-md-6 mb-4">
+            <div class="card text-white">
+                <img src="../mv_image/${item.img}" class="card-img" alt="${item.name}">
+                <div class="card-img-overlay d-flex flex-column justify-content-center">
+                    <p class="mv_card_title">${item.name}</p>
+                    ${item.showFinalTouch ? `
+                        <p class="mv_add_the">
+                            Add the <span style="color: #CF2A2A;">FINAL</span> touch to your 
+                            <span style="color: #CF2A2A;">ADVENTURE</span>
+                        </p>
+                    ` : ""}
+                    ${item.description ? `<p class="mv_run_to">${item.description}</p>` : ""}
+                    <div class="mv_explore_shop_more">
+                    <a href="/darshit/${item.link}.html" class="r_shopnowbtn">${item.btn}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}).join("");
+
+document.getElementById("dk_explore_more_box").innerHTML = sportswer_ex_box;
